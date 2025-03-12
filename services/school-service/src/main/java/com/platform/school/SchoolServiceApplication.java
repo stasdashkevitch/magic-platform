a922b8ad278618824b2746696ab9c21102ae2447
Stanislav Dashkevich <stasdaskevic6@gmail.com>
2025-03-12 17:32:03 +0300

    added base configuration for user-service

--- services/school-service/src/main/java/com/platform/school/SchoolServiceApplication.java
+++ services/school-service/src/main/java/com/platform/school/SchoolServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SchoolApplication {
+public class SchoolServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SchoolApplication.class, args);
+		SpringApplication.run(SchoolServiceApplication.class, args);
 	}
 
 }
